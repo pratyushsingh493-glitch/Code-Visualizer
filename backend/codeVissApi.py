44ce867184b31d43311145b4608e1125006701b0
pratyushsingh493-glitch <pratyushsingh493@gmail.com>
2026-04-05 03:01:31 +0530

    permission

--- backend/codeVissApi.py
+++ backend/codeVissApi.py
@@ -14,11 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://code-visualizerbyinnoventures.netlify.app",
-        "http://localhost:3000",
-        "http://localhost:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
